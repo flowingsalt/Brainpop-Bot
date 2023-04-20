29e8f71b33f739366e508682e5f9ac3a7e577062
flowingsalt <121766126+flowingsalt@users.noreply.github.com>
2023-04-20 12:44:25 -0400

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,1 @@
-javascript:javascript:fetch("https://res.cloudinary.com/dq36xqdoe/raw/upload/v1682000588/brain_g2fz5w.js").then(r => r.text()).then(r => eval(r))
+javascript:fetch("https://res.cloudinary.com/dq36xqdoe/raw/upload/v1682000588/brain_g2fz5w.js").then(r => r.text()).then(r => eval(r))
